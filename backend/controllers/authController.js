622a32b08772d4277dac47f468bbd49964c98cf3
wasanjith <wasakodi79@gmail.com>
2025-01-16 13:01:45 +0530

    Create routes and controllers to login logout and signup functions.

---
+++ backend/controllers/authController.js
@@ -0,0 +1,9 @@
+export const signup = async(req, res) => {
+    res.send("Sign up route called");
+}
+export const login = async(req, res) => {
+    res.send("Login route called");
+}
+export const logout = async(req, res) => {
+    res.send("Logout route called");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+    console.log("Server is running on http://localhost:" + PORT);
 });
