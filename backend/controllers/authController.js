fa106e4526e0dd7d0442bfd72876d28456fa0f05
wasanjith <wasakodi79@gmail.com>
2025-01-21 19:47:27 +0530

    Login backend part created successfully with authentication.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,37 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+const generateToken = (userId) => {
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    })
+
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    })
+
+    return { accessToken, refreshToken }
+}
+
+const storeRefreshToken = async(userId, refreshToken) => {
+    await redis.set(`refresh_token: ${userId}`, refreshToken, "EX", 7 * 24 * 60 * 60); //7 days
+}
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, // prevent xss attacks, cross site scripting attack
+        secure:process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevent CSRF attack, cross-site request frogery attack
+        maxAge: 15 * 60 * 1000, // 15 minutes
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // prevent xss attacks, cross site scripting attack
+        secure:process.env.NODE_ENV === "production",
+        sameSite: "strict", // prevent CSRF attack, cross-site request frogery attack
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+};
 
 export const signup = async(req, res) => {
     const { email, password, name } = req.body;
@@ -27,10 +60,21 @@ export const signup = async(req, res) => {
 
         // Create a new user
         const user = await User.create({ name, email, password });
+        //  Authenticate the user 
 
-        res.status(201).json({
-            success: true,
-            user,
+        const { accessToken, refreshToken } = generateToken(user._id);
+        await storeRefreshToken(user._id, refreshToken);
+
+        setCookies(res, accessToken, refreshToken);
+
+        res.status(201).json({  
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+
+            }, 
             message: "User created successfully",
         });
     } catch (error) {
