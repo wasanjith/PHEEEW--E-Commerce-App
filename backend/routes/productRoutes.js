f30d934e98dee3111516aba2d423ad198c83c0b3
wasanjith <wasakodi79@gmail.com>
2025-01-22 12:33:28 +0530

    Backend part done of the products with all CRUD operations!

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getAllProducts } from '../controllers/productController.js';
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedeProduct} from '../controllers/productController.js';
 import { protectRoute, adminRoute } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/category/:category",  getProductsByCategory);
+router.get("/recommendations",  getRecommendedProducts);
+router.post("/", protectRoute, adminRoute, createProduct); // Shift + Alt + Down Arrow to duplicate a line
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedeProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 
 export default router;
