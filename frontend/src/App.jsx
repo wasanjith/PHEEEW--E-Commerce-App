9abc73900e60caa18d514fdb5d13380eeb7b5de6
wasanjith <wasakodi79@gmail.com>
2025-01-30 16:55:08 +0530

    successfully created UI of admin page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import LoadingSpinner from './components/LoadingSpinner';
+import AdminPage from "./pages/AdminPage";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { useUserStore } from "./stores/useUserStore";
@@ -31,6 +32,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to='/' />} />
           <Route path="/login" element={!user ? <LoginPage/> : <Navigate to='/' />} />
+          <Route path="/secret-dashboard" element={user?.role === "admin" ? <AdminPage/> : <Navigate to='/login' />} />
         </Routes>
           </div>
           <Toaster/>
