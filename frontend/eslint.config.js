15d359f1d179c61d1e3d712876405e12bd44001f
wasanjith <wasakodi79@gmail.com>
2025-01-30 16:26:02 +0530

    Items fetch to home page from hard coded not from the backend.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -32,7 +32,8 @@ export default [
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
-      ],
+        ],
+      'react/prop-types': 'off',
     },
   },
 ]
