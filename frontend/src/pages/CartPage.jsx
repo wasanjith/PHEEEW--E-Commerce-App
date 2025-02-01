2eeafacc894904996cf4c2a949f0c7578106ce05
wasanjith <wasakodi79@gmail.com>
2025-02-01 22:58:42 +0530

    Cart items total calculating section created successfully.

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -5,6 +5,8 @@ import { ShoppingCart } from "lucide-react";
 import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
 import PeopleAlsoBought from "../components/PeopleAlsoBought";
+import OrderSummary from "../components/OrderSummary";
+import GiftCouponCard from "../components/GiftCouponCard";
 
 const CartPage = () => {
   const { cart } = useCartStore();
@@ -28,7 +30,19 @@ const CartPage = () => {
               </div>
             )}
             {cart.length > 0 && <PeopleAlsoBought />}
-          </motion.div>
+                  </motion.div>
+                  
+                  {cart.length > 0 && (
+                      <motion.div
+                      className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
+                      initial={{ opacity: 0, x: 20 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5, delay: 0.4 }}
+                  >
+                      <OrderSummary />
+                      <GiftCouponCard />
+                  </motion.div>
+                  )}
         </div>
       </div>
     </div>
--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -56,7 +56,8 @@ export const useCartStore = create((set, get) => ({
     removeFromCart: async (productId) => {
 		await axios.delete(`/cart`, { data: { productId } });
 		set((prevState) => ({ cart: prevState.cart.filter((item) => item._id !== productId) }));
-		get().calculateTotals();
+        get().calculateTotals();
+        toast.success("Product removed from cart");
     },
     
     updateQuantity: async (productId, quantity) => {
